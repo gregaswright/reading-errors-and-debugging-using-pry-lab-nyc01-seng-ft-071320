fc884ce7e7236bc327f0975ea1ced47b4aef3a29
Gregory A.S. Wright <geslim@gmail.com>
2020-06-18 18:33:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,9 +2,8 @@ require "pry"# don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    
-    s = 10 * "s" + string
-    s
+    "s" * 10 + string
+
 
   else
     string
