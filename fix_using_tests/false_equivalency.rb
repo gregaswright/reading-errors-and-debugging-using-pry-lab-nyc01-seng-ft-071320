a52752aa9ee87f5d6c728d83efe64f257f238e8e
Gregory A.S. Wright <geslim@gmail.com>
2020-06-18 17:47:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,9 +11,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num == 0
+  if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num == 1
+  elsif num = 2
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
